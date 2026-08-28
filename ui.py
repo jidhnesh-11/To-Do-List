4b8131981e89d8696a72c55868a48464e6c1cab8
Jidhnesh-11 <jidhneshpatil11@gmail.com>
2026-08-28 12:18:16 +0530

    Added frames to hold diff sec^n s

--- ui.py
+++ ui.py
@@ -6,32 +6,53 @@ import task_manger as tm #now can use CRUD functions
 
 def showWidget(window):
   
+  #header in a frame
+  header=tk.Frame(window,
+                  bg="teal",
+                  height=50)
+  header.pack(fill="x")
+  
+  #main area in a frame
+  main= tk.Frame(window,
+                 bg="#FDF4D2")
+  main.pack(fill="both",expand=True)
+  
+  #sidebar in a frame
+  sidebar= tk.Frame(main,   #main frame!!!!
+                    bg="#C8DFDB",
+                    width=150)
+  sidebar.pack(side= "left",fill="y")
+  
+  
+  #actual content!!! inside the "main frame"
+  
+  content= tk.Frame(main,
+                    bg="#FFFCE1")
+  content.pack(side="right", fill="both",expand=True)
+  
+  def show_todays_tasks():
+    print("Showing Toadys tasks")
+    
+  def show_all_tasks():
+    print("Showing all tasks")
+    
+  btn_today = tk.Button(sidebar,
+                        text=" Toaday's Tasks ",
+                        command=show_todays_tasks)
+  btn_today.pack(fill="x", pady=5)
+  
+  btn_all_tasks = tk.Button(sidebar,
+                            text="All Tasks ",
+                            command=show_all_tasks)
+  btn_all_tasks.pack(fill="x", pady=5)
+  
+   
   label=tk.Label(window,
                  text="To Do List",
                  bg="teal",
                  fg="black")
   label.pack(padx=10,pady=10)
-  '''
-  entry=tk.Entry(window,
-                 bg="#FDF4D2"
-                 )
-  entry.pack()
-  '''
-  '''
-  def on_add_click():
-    
-    title= entry.get().strip()
-    
-    if title:
-      
-      tm.addTask(title) #add task
-      
-      entry.delete(0, tk.END) #clear the entry
-      
-      all_tasks ="\n".join(tm.getTasks()) #to display the list thats contains all the task in tm
-      
-      display_label.config(text=all_tasks)
-  '''
+
   
 
   
